6084714ef0b915bf6c6278e0fb81550ac0e2f5d4
Zalim Bashorov <Zalim.Bashorov@jetbrains.com>
2014-01-29 22:09:51 +0400

    #rewrite test for indexOf

--- js/js.translator/testFiles/java/arrayList/cases/indexOf.kt
+++ js/js.translator/testFiles/java/arrayList/cases/indexOf.kt
@@ -13,33 +13,22 @@ fun listOf<T>(vararg a: T): List<T> {
     return list
 }
 
- fun box(): String {
+fun test<T>(list: List<T>, elements: List<T>, expected: List<Int>, method: List<T>.(T)->Int, methodName: String): String? {
+    for (i in 0..elements.size() - 1) {
+        val actual = list.method(elements[i])
+        if (actual != expected[i]) return "$methodName failed when find: ${elements[i]}, expected: ${expected[i]}, actual: $actual"
+    }
+    return null
+}
+
+fun box(): String {
+     //                0   1    2   3    4   5    6   7     8    9
      val list = listOf(3, "2", -1, null, 0, "2", -1, null,  0,   8)
-     val data = listOf(3, "2", -1, null, 0,   8,  5,  "3", 77, -15)
-     val indexes  = listOf(4, 1,  6, 0,  7, 5,  9, 3,  8, 2)
-     val inList = 0..5
+     val findingElements     = listOf(77, null, 0, 8, -15, "2", 5, 3, -1, "3")
+     val expectedIndexOf     = listOf(-1,    3, 4, 9,  -1,   1,-1, 0,  2,  -1)
+     val expectedLastIndexOf = listOf(-1,    7, 8, 9,  -1,   5,-1, 0,  6,  -1)
 
-     for (i in indexes) {
-         val expected =
-                 if (i == inList.start) 0
-                 else if (i == inList.end) list.size() - 1
-                 else if (i in inList ) i
-                 else -1
-
-         val actual = list.indexOf(data[i])
-         if (actual != expected) return "indexOf failed when find: ${data[i]}, expected: $expected, actual: $actual, data[$i]=${data[i]}, list: $list"
-     }
-
-     for (i in indexes) {
-         val expected =
-                 if (i == inList.start) 0
-                 else if (i == inList.end) list.size() - 1
-                 else if (i in inList ) i + inList.end - 1
-                 else -1
-
-         val actual = list.lastIndexOf(data[i])
-         if (actual != expected) return "lastIndexOf failed when find: ${data[i]}, expected: $expected, actual: $actual, data[$i]=${data[i]}, list: $list"
-     }
-
-     return "OK"
+     return test(list, findingElements, expectedIndexOf, { indexOf(it) }, "indexOf") ?:
+            test(list, findingElements, expectedLastIndexOf, { lastIndexOf(it) }, "lastIndexOf") ?:
+            "OK"
  }
